b99ceb92a9b262ebfc305ec6786c25fed066694e
Sam Alternative <sam@MacBook-Pro-5.local>
2017-11-26 12:10:06 +0000

    allowing users to update their own password, encrypting passwords in the db

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 from security import authenticate, identity # our other file
-from resources.user import UserRegister
+from resources.user import UserRegister, UserResource
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') # allows to default to sqlite for testing
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # This turns off the flask sqlalchemy modification tracker, and uses the SQLalchemy one instead
+app.config['DEBUG'] = True
 app.secret_key = 'jose'
 api = Api(app)
 
@@ -21,8 +22,16 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(StoreList, '/stores')
+api.add_resource(UserResource, '/user')
 
 if __name__ == '__main__': # this prevents running on import, as opposed to when executing directly
-	from db import db # importing thi here prevents multiple libs importing the same thing
+	from db import db # importing this here prevents multiple libs importing the same thing
 	db.init_app(app)
-	app.run(port=5000, debug=True)
+ 
+ 	# This is needed for a dev environment
+	if app.config['DEBUG']:
+		@app.before_first_request
+		def create_tables():
+			db.create_all()
+ 
+	app.run(port=5000)
