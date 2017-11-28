ac2e0d5d1d0fede8fb0daa530d1445b9d205d979
Sam Alternative <sam@MacBook-Pro-5.local>
2017-11-28 22:34:23 +0000

    adding jwt error handler

--- run.py
+++ run.py
@@ -1,18 +1,6 @@
 from app import app
 from db import db
 
-def get_http_exception_handler(app):
-    """Overrides the default http exception handler to return JSON."""
-	handle_http_exception = app.handle_http_exception
-	@wraps(handle_http_exception)
-    def ret_val(exception):
-		exc = handle_http_exception(exception)    
-		return jsonify({'code':exc.code, 'message':exc.description}), exc.code
-	return ret_val
-
-# Override the HTTP exception handler.
-app.handle_http_exception = get_http_exception_handler(app)
-
 db.init_app(app)
 
 
